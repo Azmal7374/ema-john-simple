9cf7fcfa33cf32d5d2190edfb2eaf68dc662d0da
Azmal <azmalgazi8@gmail.com>
2023-03-28 02:27:25 +0600

    retrove product from stored cart it ti cart stale

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,10 +16,49 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, [])
 
+    // useEffect(() => {
+    //     // console.log('products', products);
+    //     const storedCart = getShoppingCart();
+    //     // console.log(storedCart);
+    //     // step1: get id
+    //     for(const id in storedCart) {
+    //         // console.log(id);
+
+    //         //step2:get the product using id
+    //         const addedProduct = products.find(product => product.id === id)
+    //         console.log(addedProduct);
+
+    //         //step3: get quantity of the product
+    //         const quantity = storedCart[id];
+    //         // console.log(quantity);
+    //         addedProduct.quantity = quantity;
+    //         console.log(addedProduct);
+    //     }
+    // }, [products])
+
+
     useEffect(() => {
+
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    }, [])
+        const savedCart = [];
+
+        //step1: get id of the addedproduct
+        for(const id in storedCart) {
+            //step2: get  product from produtcs state by using id
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+              //step3: add quantity
+              const quantity = storedCart[id];
+              addedProduct.quantity = quantity;
+
+              //step4: add the added product to the saved cart
+              savedCart.push(addedProduct);
+            }
+            // console.log(addedProduct);
+        }
+        //step5:  set the card
+        setCart(savedCart)
+    },[products])
 
     const handleAddToCart = (product) => {
         //    console.log('product added');
