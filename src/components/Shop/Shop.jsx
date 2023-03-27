8c347e59be3b4faae3c5354fac39b71cea86da1b
Azmal <azmalgazi8@gmail.com>
2023-03-27 19:30:50 +0600

    productcart added & count purchase product

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,6 +5,7 @@ import './Shop.css'
 const Shop = () => {
 
     const [products, setProducts] = useState([])
+    const [cart,  setCart] = useState([])
 
     useEffect(() => {
         fetch('products.json')
@@ -12,17 +13,25 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, [])
 
+    const handleAddToCart = (product) => {
+        //    console.log('product added');
+        //    console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+        }
+
     return (
         <div className="shop-container">
         <div className="products-container">
        
         {
-            products.map(product => <Product product={product} key={product.id}></Product>)
+            products.map(product => <Product product={product} key={product.id} handleAddToCart={handleAddToCart}></Product>)
         }
         </div>
 
         <div className="card-container">
           <h4>Oreder summary</h4>
+          <p>Selected Items: {cart.length}</p>
         </div>
         </div>
     );
